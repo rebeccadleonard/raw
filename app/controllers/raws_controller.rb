8c3f4b0c3487c7cfd639e0fe17fe0bdace903074
Rebecca Leonard <rebeccadleonard@gmail.com>
2017-05-30 04:48:58 +0000

    welcome page

---
+++ app/controllers/raws_controller.rb
@@ -0,0 +1,6 @@
+class RawsController < ApplicationController
+
+  def index
+  end
+  
+end
---
+++ test/controllers/raws_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RawsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
